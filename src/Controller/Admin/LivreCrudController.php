30208c35303f90c2ddc4437b0891b50f31ff621b
MILOUD-BENAISSA <miloud.benaissa151290@gmail.com>
2023-06-21 13:21:07 +0200

    upload fichier live back

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Livre;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class LivreCrudController extends AbstractCrudController
 {
@@ -15,13 +19,18 @@ class LivreCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield from parent::configureFields($pageName);
+        // yield from parent::configureFields($pageName);
+        
+        yield TextField::new('titre');
+        yield TextField::new('isbn');
+        yield TextareaField::new('description');
+
+        yield BooleanField::new('archive');
+
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class);
+
         yield AssociationField::new('auteur');
         yield AssociationField::new('genres');
         yield AssociationField::new('editeur');
-       
-    
     }
-
-
 }
